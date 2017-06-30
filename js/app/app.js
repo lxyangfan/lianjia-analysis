544514e3feeb758e1f0958287f71b33ddb8c28ec
fr4nk.yang <fr4nk.yang@gmail.com>
2017-06-30 20:38:35 +0800

    数据在百度echarts+地图显示

--- js/app/app.js
+++ js/app/app.js
@@ -7,11 +7,12 @@ define(['jquery', 'echarts', 'bmap',
 ) {
 
     function run() {
-        console.log("各位观众");
+
+        console.log("开始运行");
         $(document).ready(function() {
-            console.log("大家好");
-            console.log(props);
-            dd = props.map(function(e, i){
+            console.log("DocumentReady...");
+
+            dd = props.map(function(e, i) {
                 o = {};
                 value = [];
                 value[0] = e["lng"];
@@ -22,28 +23,121 @@ define(['jquery', 'echarts', 'bmap',
                 return o;
             });
 
-            console.debug(dd);
+            console.debug("数据ready")
 
             var bmapChart = echarts.init(document.getElementById('map-wrap'));
-            var myData = [{
-                name: '海门',
-                value: [121.15, 31.89, 90]
-            }, {
-                name: '鄂尔多斯',
-                value: [109.781327, 39.608266, 120]
-            }, {
-                name: '招远',
-                value: [120.38, 37.35, 142]
-            }, {
-                name: '舟山',
-                value: [122.207216, 29.985295, 123]
-            }, ]
 
             var option = {
+                tooltip: {
+                    trigger: 'item',
+                    formatter: function(param) {
+                        // 显示保留2位小数的房价
+                        return param.name + ' : ' + Math.round(param.data.value[2] * 100) / 100 + ' 万/平';
+                    }
+                },
                 bmap: {
                     center: [121.452058472, 31.2294031411],
                     zoom: 10,
                     roam: true,
+                    mapStyle: {
+                        styleJson: [{
+                            'featureType': 'water',
+                            'elementType': 'all',
+                            'stylers': {
+                                'color': '#d1d1d1'
+                            }
+                        }, {
+                            'featureType': 'land',
+                            'elementType': 'all',
+                            'stylers': {
+                                'color': '#f3f3f3'
+                            }
+                        }, {
+                            'featureType': 'railway',
+                            'elementType': 'all',
+                            'stylers': {
+                                'visibility': 'off'
+                            }
+                        }, {
+                            'featureType': 'highway',
+                            'elementType': 'all',
+                            'stylers': {
+                                'color': '#fdfdfd'
+                            }
+                        }, {
+                            'featureType': 'highway',
+                            'elementType': 'labels',
+                            'stylers': {
+                                'visibility': 'off'
+                            }
+                        }, {
+                            'featureType': 'arterial',
+                            'elementType': 'geometry',
+                            'stylers': {
+                                'color': '#fefefe'
+                            }
+                        }, {
+                            'featureType': 'arterial',
+                            'elementType': 'geometry.fill',
+                            'stylers': {
+                                'color': '#fefefe'
+                            }
+                        }, {
+                            'featureType': 'poi',
+                            'elementType': 'all',
+                            'stylers': {
+                                'visibility': 'off'
+                            }
+                        }, {
+                            'featureType': 'green',
+                            'elementType': 'all',
+                            'stylers': {
+                                'visibility': 'off'
+                            }
+                        }, {
+                            'featureType': 'subway',
+                            'elementType': 'all',
+                            'stylers': {
+                                'visibility': 'off'
+                            }
+                        }, {
+                            'featureType': 'manmade',
+                            'elementType': 'all',
+                            'stylers': {
+                                'color': '#d1d1d1'
+                            }
+                        }, {
+                            'featureType': 'local',
+                            'elementType': 'all',
+                            'stylers': {
+                                'color': '#d1d1d1'
+                            }
+                        }, {
+                            'featureType': 'arterial',
+                            'elementType': 'labels',
+                            'stylers': {
+                                'visibility': 'off'
+                            }
+                        }, {
+                            'featureType': 'boundary',
+                            'elementType': 'all',
+                            'stylers': {
+                                'color': '#fefefe'
+                            }
+                        }, {
+                            'featureType': 'building',
+                            'elementType': 'all',
+                            'stylers': {
+                                'color': '#d1d1d1'
+                            }
+                        }, {
+                            'featureType': 'label',
+                            'elementType': 'labels.text.fill',
+                            'stylers': {
+                                'color': '#999999'
+                            }
+                        }]
+                    }
                 },
                 visualMap: { // 视觉映射组件
                     type: 'continuous',
@@ -54,75 +148,103 @@ define(['jquery', 'echarts', 'bmap',
                         color: ['#50a3ba', '#eac736', '#d94e5d']
                     },
                     textStyle: {
-                        color: '#fff'
+                        color: '#000'
                     }
                 },
-                series: [
-                {
-                    name: '销量', // series名称
-                    type: 'scatter', // series图表类型
-                    coordinateSystem: 'bmap', // series坐标系类型
-                    data: dd
+                series: [{
+                    name: '6月均价',
+                    type: 'effectScatter',
+                    coordinateSystem: 'bmap',
+                    data: dd,
+                    symbolSize: function(val) {
+                        return val[2]*1.3;
+                    },
+                    showEffectOn: 'render',
+                    rippleEffect: {
+                        brushType: 'stroke'
+                    },
+                    hoverAnimation: true,
+                    label: {
+                        normal: {
+                            formatter: function(param) {
+                                // 显示保留2位小数的房价
+                                return param.name + '' + Math.round(param.data.value[2] * 100) / 100 + '万';
+                            },
+                            position: 'top',
+                            show: false
+                        }
+                    },
+                    itemStyle: {
+                        normal: {
+                            color: 'purple',
+                            shadowBlur: 10,
+                            shadowColor: '#333'
+                        }
+                    },
+                    zlevel: 1
                 }],
                 mapStyle: { // 百度地图自定义样式
-                  styleJson: [
-                      // 陆地
-                      {
-                          "featureType": "land",
-                          "elementType": "all",
-                          "stylers": {
-                              "color": "#073763"
-                          }
-                      },
-                      // 水系
-                      {
-                          "featureType": "water",
-                          "elementType": "all",
-                          "stylers": {
-                              "color": "#073763",
-                              "lightness": -54
-                          }
-                      },
-                      // 国道与高速
-                      {
-                          "featureType": "highway",
-                          "elementType": "all",
-                          "stylers": {
-                              "color": "#45818e"
-                          }
-                      },
-                      // 边界线
-                      {
-                          "featureType": "boundary",
-                          "elementType": "all",
-                          "stylers": {
-                              "color": "#ffffff",
-                              "lightness": -62,
-                              "visibility": "on"
-                          }
-                      },
-                      // 行政标注
-                      {
-                          "featureType": "label",
-                          "elementType": "labels.text.fill",
-                          "stylers": {
-                              "color": "#ffffff",
-                              "visibility": "on"
-                          }
-                      }, {
-                          "featureType": "label",
-                          "elementType": "labels.text.stroke",
-                          "stylers": {
-                              "color": "#444444",
-                              "visibility": "on"
-                          }
-                      }
-                  ]
-              }
+                    styleJson: [
+                        // 陆地
+                        {
+                            "featureType": "land",
+                            "elementType": "all",
+                            "stylers": {
+                                "color": "#073763"
+                            }
+                        },
+                        // 水系
+                        {
+                            "featureType": "water",
+                            "elementType": "all",
+                            "stylers": {
+                                "color": "#073763",
+                                "lightness": -54
+                            }
+                        },
+                        // 国道与高速
+                        {
+                            "featureType": "highway",
+                            "elementType": "all",
+                            "stylers": {
+                                "color": "#45818e"
+                            }
+                        },
+                        // 边界线
+                        {
+                            "featureType": "boundary",
+                            "elementType": "all",
+                            "stylers": {
+                                "color": "#ffffff",
+                                "lightness": -62,
+                                "visibility": "on"
+                            }
+                        },
+                        // 行政标注
+                        {
+                            "featureType": "label",
+                            "elementType": "labels.text.fill",
+                            "stylers": {
+                                "color": "#ffffff",
+                                "visibility": "on"
+                            }
+                        }, {
+                            "featureType": "label",
+                            "elementType": "labels.text.stroke",
+                            "stylers": {
+                                "color": "#444444",
+                                "visibility": "on"
+                            }
+                        }
+                    ]
+                }
             };
 
             bmapChart.setOption(option);
+            // var bmap = bmapChart.getModel().getComponent('bmap').getBMap(); // 百度地图实例
+            // bmap.setMapStyle({ style: 'dark' });
         });
+
 
     }
 
